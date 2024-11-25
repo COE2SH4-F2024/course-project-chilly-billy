fd0fa5dc1ec3489c20ab24651fcb5b16dcc4d160
Adrian Ali <ali247@mcmaster.ca>
2024-11-25 13:35:59 -0500

    ITERATION 2A

--- objPosArrayList_TestSuite/objPosArrayList.cpp
+++ objPosArrayList_TestSuite/objPosArrayList.cpp
@@ -5,50 +5,94 @@
 
 objPosArrayList::objPosArrayList()
 {
-    
+    arrayCapacity = 200; // default
+
+    aList = new objPos[arrayCapacity];
+    listSize = 0;
+    //aList
+
 }
+
+
 
 objPosArrayList::~objPosArrayList()
 {
-
+    delete[] aList; // CHECK FOR LEAKS
 }
+
+// add 2 more for rule of 4
 
 int objPosArrayList::getSize() const
 {
-
+    return listSize;
 }
 
 void objPosArrayList::insertHead(objPos thisPos)
 {
-    
+    if(arrayCapacity > listSize)
+    {
+        objPos* newlist = new objPos[arrayCapacity];
+        newlist[0] = thisPos;
+        for(int i=0; i < listSize; i++)
+        {
+            newlist[i+1] = aList[i];
+        }
+        delete[] aList;
+        aList = newlist;
+        listSize++;
+    }
 }
 
 void objPosArrayList::insertTail(objPos thisPos)
 {
-    
+    if(arrayCapacity > listSize)
+    {
+        aList[listSize] = thisPos; // listsize-1 is last element in the list, + 1 is appending
+        listSize++;
+    }
 }
 
 void objPosArrayList::removeHead()
 {
-    
+    if(listSize > 0)
+    {
+        objPos* newlist = new objPos[arrayCapacity];
+        for(int i=0; i < listSize-1; i++)
+        {
+            newlist[i] = aList[i+1];
+        }
+        delete[] aList;
+        aList = newlist;
+        listSize--;
+    }
 }
 
 void objPosArrayList::removeTail()
 {
-    
+    if(listSize > 0)
+    {
+        objPos* newlist = new objPos[arrayCapacity];
+        for(int i=0; i < listSize-1; i++)
+        {
+            newlist[i] = aList[i];
+        }
+        delete[] aList;
+        aList = newlist;
+        listSize--;
+    }
 }
 
 objPos objPosArrayList::getHeadElement() const
 {
-    
+    return aList[0];   
 }
 
 objPos objPosArrayList::getTailElement() const
 {
-    
+    return aList[listSize-1];
 }
 
 objPos objPosArrayList::getElement(int index) const
 {
-    
+    return aList[index];
 }
